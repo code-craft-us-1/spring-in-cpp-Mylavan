481eb5d694bbe499b34ebe6d9a2e6d000f8d900b
Mylavan <124572058+Mylavan@users.noreply.github.com>
2024-08-20 14:21:07 +0530

    Test1

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,27 @@
 #include "stats.h"
+#include <iostream>
+using namespace Statistics;
+using namespace std;
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+ computedStatss ComputeStatistics(const std::vector<double>&vec ) {
+     double sum = 0;
+     double min = vec[0];
+     double max = vec[0];
+    for (int i = 0; i < vec.size(); i++)
+    {
+        sum = sum + vec[i];
+        if (min > vec[i])
+        {
+            min = vec[i];
+        }
+        if (max < vec[i])
+        {
+            max = vec[i];
+        }
+    }
+    computedStatss a;
+    a.average = sum / vec.size();
+    a.max = max;
+    a.min = min;
+    return a;
 }
--- stats.h
+++ stats.h
@@ -2,6 +2,11 @@
 
 namespace Statistics {
     // define the Stats structure here. See the tests to infer its properties
-    
-    Stats ComputeStatistics(const std::vector<___>& );
+    struct computedStatss
+    {
+        long average=0;
+        long max=0;
+        long min=0;
+    };
+    computedStatss ComputeStatistics(const std::vector<double>&vec);
 }
