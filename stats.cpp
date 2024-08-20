402b515b006c78399daf6b1e6cecc044058b8643
Mylavan <124572058+Mylavan@users.noreply.github.com>
2024-08-20 17:09:18 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -3,26 +3,43 @@
 using namespace Statistics;
 using namespace std;
 
- computedStatss ComputeStatistics(const std::vector<double>&vec ) {
-     double sum = 0;
-     double min = vec[0];
-     double max = vec[0];
-    for (int i = 0; i < vec.size(); i++)
-    {
-        sum = sum + vec[i];
-        if (min > vec[i])
-        {
-            min = vec[i];
-        }
-        if (max < vec[i])
-        {
-            max = vec[i];
-        }
-    }
-    computedStatss a;
-    a.average = sum / vec.size();
-    a.max = max;
-    a.min = min;
-  std::cout<<(a.average)<<std::endl;
-    return a;
-}
+// computedStatss ComputeStatistics(const std::vector<double>&vec ) {
+//     double sum = 0;
+//     double min = vec[0];
+//     double max = vec[0];
+//    for (int i = 0; i < vec.size(); i++)
+//    {
+//        sum = sum + vec[i];
+//        if (min > vec[i])
+//        {
+//            min = vec[i];
+//        }
+//        if (max < vec[i])
+//        {
+//            max = vec[i];
+//        }
+//    }
+//    computedStatss a;
+//    a.average = sum / vec.size();
+//    a.max = max;
+//    a.min = min;
+//    return a;
+//}
+
+ computedStatss Statistics::ComputeStatistics(const std::vector<double>& vec)
+ {
+     computedStatss stats;
+
+     if (vec.empty()) {
+         stats.average = 0.0;
+         stats.min = 0.0;
+         stats.max = 0.0;
+     }
+     else {
+         stats.average = std::accumulate(vec.begin(), vec.end(), 0.0) / vec.size();
+         stats.min = *std::min_element(vec.begin(), vec.end());
+         stats.max = *std::max_element(vec.begin(), vec.end());
+     }
+
+     return stats;
+ }
