e6e3b22d2ccc41f2bf18be3e5023f97a720214bd
Mylavan <124572058+Mylavan@users.noreply.github.com>
2024-08-20 17:10:06 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,12 +1,12 @@
 #include <vector>
-
+#include <numeric>
 namespace Statistics {
     // define the Stats structure here. See the tests to infer its properties
     struct computedStatss
     {
-        long average=0;
-        long max=0;
-        long min=0;
+        double average=0;
+        double max=0;
+        double min=0;
     };
     computedStatss ComputeStatistics(const std::vector<double>&vec);
 }
